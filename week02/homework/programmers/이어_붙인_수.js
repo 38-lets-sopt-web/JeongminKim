22d2e4c875608c2b5aecd973c49f0f685ab3b5be
jjangminii <wjdals030328@catholic.ac.kr>
2026-04-18 18:14:12 +0900

    코드 수정

--- week02/homework/programmers/이어_붙인_수.js
+++ week02/homework/programmers/이어_붙인_수.js
@@ -2,6 +2,5 @@ function solution(num_list) {
   var odd = "",
     eve = "";
   num_list.forEach((n) => (n % 2 == 0 ? (eve += n) : (odd += n)));
-  var answer = Number(odd) + Number(eve);
-  return answer;
+  return Number(odd) + Number(eve);
 }
