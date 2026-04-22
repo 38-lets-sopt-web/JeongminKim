4c21906b35b769259ac984131413154089795d5d
jjangminii <wjdals030328@catholic.ac.kr>
2026-04-22 16:22:19 +0900

    chore: 주석 추가

--- week02/homework/project/js/data.js
+++ week02/homework/project/js/data.js
@@ -1,15 +1,18 @@
 import { expenses } from "./expenses.js";
 
+// 초기 데이터 설정
 export function initData() {
   if (!localStorage.getItem("expenseData")) {
     localStorage.setItem("expenseData", JSON.stringify(expenses));
   }
 }
 
+// 데이터 가져오기
 export function getTransactions() {
   return JSON.parse(localStorage.getItem("expenseData"));
 }
 
+// 데이터 저장하기
 export function saveTransactions(transactions) {
   localStorage.setItem("expenseData", JSON.stringify(transactions));
 }
